341ec80ca8446f1b89e37402f8eb605de34e82b7
davidyoon <qqpo12@naver.com>
2022-05-01 23:29:58 +0900

    add koa static mount route

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 const Koa = require('koa')
 const Pug = require('koa-pug')
 const path = require('path')
+const route = require('koa-route')
+const serve = require('koa-static')
+const mount = require('koa-mount')
+const websockify = require('koa-websocket')
 
-const app = new Koa()
+// the magic happens right here
+const app = websockify(new Koa())
+
+app.use(mount('/public', serve('src/public')))
 
 // @ts-ignore
 // eslint-disable-next-line no-new
@@ -16,5 +23,14 @@ new Pug({
 app.use(async (ctx) => {
   await ctx.render('main')
 })
+
+app.ws.use(
+  route.all('/test/:id', (ctx) => {
+    ctx.websocket.send('Hello World')
+    ctx.websocket.on('message', (message) => {
+      console.log(message)
+    })
+  })
+)
 
 app.listen(5005)
